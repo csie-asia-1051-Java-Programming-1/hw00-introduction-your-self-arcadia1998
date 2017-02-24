e30767aa6a6711b4e6b5488a634366bd805eb8d8
arcadia1998 <105021020@live.asia.edu.tw>
2017-02-24 15:16:55 +0800

    Update hw00.java

--- src/hw00.java
+++ src/hw00.java
@@ -6,7 +6,7 @@ public class hw00 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("學號:105021020\r"+"姓名:姚俊榮\r");
+		System.out.print("學號:105021020\r"+"姓名:姚俊榮");
 	}
 
 }
