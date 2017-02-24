69193dce200ac62ef7e4adc933885cff091e59d2
arcadia1998 <jim280557>
2017-02-24 15:08:30 +0800

    hw00 submit

---
+++ src/hw00.java
@@ -0,0 +1,12 @@
+/*
+* Date: 2017/02/24
+* Author: 105021020 姚俊榮
+*/
+public class hw00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("學號:105021020\r"+"姓名:姚俊榮\r");
+	}
+
+}
